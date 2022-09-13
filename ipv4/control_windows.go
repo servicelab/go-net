411dfe2ec16738a7b02c53f755a245762b59942a
Aman Karmani <aman@tmm1.net>
2022-09-12 18:38:29 -0700

    match upstream style

--- ipv4/control_windows.go
+++ ipv4/control_windows.go
@@ -4,9 +4,7 @@
 
 package ipv4
 
-import (
-	"golang.org/x/net/internal/socket"
-)
+import "golang.org/x/net/internal/socket"
 
 func setControlMessage(c *socket.Conn, opt *rawOpt, cf ControlFlags, on bool) error {
 	opt.Lock()
